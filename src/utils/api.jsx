444a4bb12ead4d7d2b4f65231bed2bc250262312
Arique <arique1104@gmail.com>
2025-04-20 10:32:21 -0700

    creates growth summary function

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -17,3 +17,8 @@ export default instance
 export async function createPIESCheckin(userId, data) {
     return await instance.post(`/users/${userId}/pies_entries`, data)
 }
+
+export async function fetchGrowthSummary() {
+    const response = await instance.get("/growth_summary");
+    return response.data;
+}
